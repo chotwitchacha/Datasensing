841572b1a40071afef4a9be4c9f2bb6ab7cf0414
chotwitchacha <90165442+chotwitchacha@users.noreply.github.com>
2023-05-10 12:43:39 +0700

    login page

---
+++ pages/main/index.tsx
@@ -0,0 +1,15 @@
+import { useRef, useState } from 'react'
+import { Button, Box, Grid, Divider } from '@mui/material'
+import { GetServerSideProps, InferGetServerSidePropsType } from 'next'
+import { useRouter } from 'next/router'
+
+
+const Main = () => {
+    
+    return(
+        <>
+        </>
+    )
+}
+
+export default Main
